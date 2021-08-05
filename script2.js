b8e7ebbc2453deb2f0397f45805f7ddbb67e5fad
Jden Redden <iam@jden.me>
2021-08-05 22:53:24 +0930

    MVP

--- script2.js
+++ script2.js
@@ -9,19 +9,47 @@ let DateTime = luxon.DateTime;
 //     output(new Date().toUTCString());
 // }, 1000); // update about every second
 
-var interval = 1000; // ms
+var interval = 60000; // ms
 var expected = Date.now() + interval;
 var localTime= DateTime.now();
-setTimeout(step, interval);
-function step() {
-    var dt = Date.now() - expected; // the drift (positive for overshooting)
-    if (dt > interval) {
-        // something really bad happened. Maybe the browser (tab) was inactive?
-        // possibly special handling to avoid futile "catch up" run
+// setTimeout(step, interval);
+// function step() {
+//     var dt = Date.now() - expected; // the drift (positive for overshooting)
+//     if (dt > interval) {
+//         // something really bad happened. Maybe the browser (tab) was inactive?
+//         // possibly special handling to avoid futile "catch up" run
+//     }
+
+
+var exchanges = {
+    "nyse" : {
+        "nameLong" : "New York Stock Exchange",
+        "nameShort" : "NYSE",
+        "timeZone" : "America/New_York",
+        "sessions" : {
+            "pre" : {
+                "openHour" : 6,
+                "openMinute" : 30,
+                "duration" : 180
+            },
+            "core" : {
+                "openHour" : 9,
+                "openMinute" : 30,
+                "duration" : 390 
+            },
+            "after" : {
+                "openHour" : 4,
+                "openMinute" : 0,
+                "duration" : 240 
+            }
+        }
     }
+}
 
-
-
+console.log("JSON Test");
+console.log(exchanges.nyse.nameLong);
+console.log(Object.keys(exchanges.nyse.sessions).length);
+console.log(exchanges.nyse.sessions.pre.openHour);
 
 localTime = localTime.plus({seconds: 1});
 
@@ -51,12 +79,31 @@ let exchangeTime = localTime.setZone("America/New_York");
     // console.log(preClose);
     
     if (exchangeTime > open && exchangeTime < close) {
-        var marketOpen = true;
+        var marketStatus = "Yes";
+        var marketStatusVerb = "closes";
+        var marketStatusTime = close.toRelative({ unit: [ "hours", "minutes"] });
     } else {
-        var marketOpen = false;
+        var marketStatus = "No";
+        var marketStatusVerb = "opens";
+        var marketStatusTime = open.toRelative({ unit: [ "hours", "minutes"] });
     }
     
-    document.getElementById("open").innerHTML = marketOpen;
+    document.getElementById("status").innerHTML = marketStatus;
+    document.getElementById("statusVerb").innerHTML = marketStatusVerb;
+    document.getElementById("statusTime").innerHTML = marketStatusTime;
+    
+    document.getElementById("coreOpen").innerHTML = open.toLocaleString(DateTime.TIME_SIMPLE);
+    document.getElementById("coreClose").innerHTML = close.toLocaleString(DateTime.TIME_SIMPLE);
+    
+    document.getElementById("localCoreOpen").innerHTML = open.setZone("Australia/Adelaide").toLocaleString(DateTime.TIME_SIMPLE);
+    document.getElementById("localCoreClose").innerHTML = close.setZone("Australia/Adelaide").toLocaleString(DateTime.TIME_SIMPLE);
+    
+    document.getElementById("localCity").innerHTML = localTime.zoneName.split("/")[1];
+    document.getElementById("exchangeCity").innerHTML = exchangeTime.zoneName.split("/")[1];
+    
+    
+    
+    
     
     document.getElementById("table").rows[1].cells.item(1).innerHTML = preOpen.toLocaleString(DateTime.TIME_SIMPLE);
     document.getElementById("table").rows[1].cells.item(2).innerHTML = preOpen.setZone("Australia/Adelaide").toLocaleString(DateTime.TIME_SIMPLE);
@@ -69,7 +116,8 @@ let exchangeTime = localTime.setZone("America/New_York");
     
     document.getElementById("table").rows[3].cells.item(1).innerHTML = open.toLocaleString(DateTime.TIME_SIMPLE);
     document.getElementById("table").rows[3].cells.item(2).innerHTML = open.setZone("Australia/Adelaide").toLocaleString(DateTime.TIME_SIMPLE);
-    document.getElementById("table").rows[3].cells.item(3).innerHTML = open.diff(exchangeTime, 'hours').toFormat('hh:mm:ss');
+    var openDiff = open.diffNow(['hours', 'minutes']);
+    document.getElementById("table").rows[3].cells.item(3).innerHTML = -openDiff.hours + " hours and " + -Math.floor(openDiff.minutes) + " minutes ago";
     
     document.getElementById("table").rows[4].cells.item(1).innerHTML = close.toLocaleString(DateTime.TIME_SIMPLE);
     document.getElementById("table").rows[4].cells.item(2).innerHTML = close.setZone("Australia/Adelaide").toLocaleString(DateTime.TIME_SIMPLE);
@@ -82,6 +130,6 @@ let exchangeTime = localTime.setZone("America/New_York");
     document.getElementById("table").rows[6].cells.item(1).innerHTML = afterClose.toLocaleString(DateTime.TIME_SIMPLE);
     document.getElementById("table").rows[6].cells.item(2).innerHTML = afterClose.setZone("Australia/Adelaide").toLocaleString(DateTime.TIME_SIMPLE);
     document.getElementById("table").rows[6].cells.item(3).innerHTML = afterClose.diff(exchangeTime, 'hours').toFormat('hh:mm:ss');
-expected += interval;
-setTimeout(step, Math.max(0, interval - dt)); // take into account drift
-}
+// expected += interval;
+// setTimeout(step, Math.max(0, interval - dt)); // take into account drift
+// }
